3b449f7d90cbbfa4c42b4cbdffcafaf4651ce7eb
Igor Koneshov <igorkoneshov@mail.ru>
2023-03-27 00:31:15 +0400

    feat: merge pages and components in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,59 +1,103 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import Form from "./Form/Form";
-import {Genre, IFilm} from "../types";
-import Films from "./Films/Films";
-import Search from "./Search/Search";
-import {defaultFilms} from "../utils/standart-film-list";
-import {Route, Routes, useNavigate} from "react-router-dom";
+import { Genre, IFilm } from "../types";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useLocalStorage } from "../hook/useLocalStorage";
+import FilmPage from "../pages/FilmPage/FilmPage";
+import FilmsPage from "../pages/FilmsPage/FilmsPage";
+import "./App.scss";
+import Header from "./Header/Header";
 
 const App: React.FC = () => {
   // Навигация
   const navigate = useNavigate();
   // Инициализирую глобальную переменную с фильмами
-  const [films, setFilms] = useState<IFilm[]>(defaultFilms);
+  const [localStorage, setLocalStorage] = useLocalStorage("films");
+  const [films, setFilms] = useState<IFilm[]>(localStorage);
   const [filteredFilms, setFilteredFilms] = useState<IFilm[]>(films);
   const [searchFilter, setSearchFilter] = useState<string>("any");
 
-  const handleFilmsChange = (films: IFilm[]) => setFilms(films);
+  const handleFilmsChange = (films: IFilm[]) => {
+    setFilms(films);
+    setLocalStorage(films);
+  };
   const handleSearch = (search: string) => {
-    const filtered = films.filter((film) =>
-      film.title.includes(search) ||
-      film.compositor.includes(search) ||
-      film.producer.includes(search) ||
-      film.genre.includes(search as Genre) ||
-      film.ageRating.includes(search)
+    const filtered = films.filter(
+      (film) =>
+        film.title.includes(search) ||
+        film.compositor.includes(search) ||
+        film.producer.includes(search) ||
+        film.genre.includes(search as Genre) ||
+        film.ageRating.includes(search)
     );
     setFilteredFilms(filtered);
-  }
+  };
   const handleDelete = (id: number) => {
     const isConfirm = confirm("Подтверждаете удаление?");
     if (isConfirm) {
       const updatedFilms = films.filter((film) => film.id !== id);
       setFilms(updatedFilms);
+      setLocalStorage(updatedFilms);
     }
-  }
+  };
 
-  const handleNavigate = () => navigate('/');
+  const [selectedFilm, setSelectedFilm] = useState<IFilm>(null);
+
+  const handleSelectFilm = (selectedFilm: IFilm) => {
+    setSelectedFilm(selectedFilm);
+    console.log(selectedFilm);
+  };
+
+  const handleNavigate = () => navigate("/");
+
+  const updateSelectedFilm = (film: IFilm) => {
+    setSelectedFilm(film);
+    const findedFilm: IFilm = localStorage.find(
+      (el: IFilm) => el.id === film.id
+    );
+    if (findedFilm) {
+      findedFilm.comments = [...film.comments];
+      setLocalStorage(localStorage);
+    }
+  };
 
   useEffect(() => {
     setFilteredFilms(films);
     setSearchFilter("");
-  }, [films])
+  }, [films]);
   return (
     <div className="page">
+      <Header />
       <Routes>
-        <Route path='/' element={
-          <Form films={films} handleUpdateFilms={handleFilmsChange}/>}/>
-        <Route path="/films" element={
-          <React.Fragment>
-            <h1 style={{textAlign: "center"}} >Фильмы</h1>
-            <Search handleSearch={handleSearch}/>
-            <Films handleDelete={handleDelete} films={filteredFilms}/>
-            <button onClick={handleNavigate}>Назад</button>
-          </React.Fragment>}/>
+        <Route path="/" element={<div>Main page</div>} />
+        <Route
+          path="/form"
+          element={<Form films={films} handleUpdateFilms={handleFilmsChange} />}
+        />
+        <Route
+          path="/films"
+          element={
+            <FilmsPage
+              handleSearch={handleSearch}
+              handleDelete={handleDelete}
+              handleNavigate={handleNavigate}
+              filteredFilms={filteredFilms}
+              handleSelectFilm={handleSelectFilm}
+            />
+          }
+        />
+        <Route
+          path="/film"
+          element={
+            <FilmPage
+              updateSelectedFilm={updateSelectedFilm}
+              selectedFilm={selectedFilm}
+            />
+          }
+        />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
